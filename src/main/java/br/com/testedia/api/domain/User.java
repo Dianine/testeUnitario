0954b86b964ab838d95590ce01991ffec6c4a770
Dianine <dianine23@gmail.com>
2022-06-13 20:29:23 -0300

    Criação da Classe User

---
+++ src/main/java/br/com/testedia/api/domain/User.java
@@ -0,0 +1,22 @@
+package br.com.testedia.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
