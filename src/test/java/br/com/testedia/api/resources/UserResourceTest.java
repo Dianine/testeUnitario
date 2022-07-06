5a02349758ee7e17e6d04b22d31a84f49b7416aa
Dianine <dianine23@gmail.com>
2022-07-06 09:14:34 -0300

    Teste endpoint findAll da classe user resourses

--- src/test/java/br/com/testedia/api/resources/UserResourceTest.java
+++ src/test/java/br/com/testedia/api/resources/UserResourceTest.java
@@ -12,12 +12,14 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserResourceTest {
@@ -26,7 +28,7 @@ class UserResourceTest {
     public static final String NAME     = "Dianine";
     public static final String EMAIL    = "dianine@gmail.com";
     public static final String PASSWORD = "123";
-
+    public static final int INDEX = 0;
 
 
     @InjectMocks
@@ -53,13 +55,31 @@ class UserResourceTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAListOfUserDTO () {
+        when(service.findAll()).thenReturn(List.of(user));
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<List<UserDTO>> response = resource.findAll();
+
+        Assertions.assertNotNull(response);
+        Assertions.assertNotNull(response.getBody());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(ResponseEntity.class, response.getClass());
+        Assertions.assertEquals(ArrayList.class, response.getBody().getClass());
+        Assertions.assertEquals(UserDTO.class, response.getBody().get(INDEX).getClass());
+
+        Assertions.assertEquals(ID, response.getBody().get(INDEX).getId());
+        Assertions.assertEquals(NAME, response.getBody().get(INDEX).getName());
+        Assertions.assertEquals(EMAIL, response.getBody().get(INDEX).getEmail());
+        Assertions.assertEquals(PASSWORD, response.getBody().get(INDEX).getPassword());
+
+
     }
 
     @Test
     void whenFindByIdThenReturnSuccess() {
-        Mockito.when(service.findById(Mockito.anyInt())).thenReturn(user);
-        Mockito.when(mapper.map(any(), any())).thenReturn(userDTO);
+        when(service.findById(Mockito.anyInt())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
 
         ResponseEntity<UserDTO> response = resource.findById(ID);
         Assertions.assertNotNull(response);
